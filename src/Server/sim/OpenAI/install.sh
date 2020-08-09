6a56a5b4f482204c9a6ac24e924288819c093ba6
Xavier Geerinck <xavier.geerinck@gmail.com>
2020-08-09 21:44:30 +0200

    Disable original OpenAI Gym

--- src/Server/sim/OpenAI/install.sh
+++ src/Server/sim/OpenAI/install.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-# Install dependencies
-apt-get install -y libglu1-mesa-dev libgl1-mesa-dev libosmesa6-dev xvfb ffmpeg curl patchelf libglfw3 libglfw3-dev cmake zlib1g zlib1g-dev swig
+# #!/bin/bash
+# # Install dependencies
+# apt-get install -y libglu1-mesa-dev libgl1-mesa-dev libosmesa6-dev xvfb ffmpeg curl patchelf libglfw3 libglfw3-dev cmake zlib1g zlib1g-dev swig
 
-# Install OpenAI in /opt (which is meant for OS unbundled packages)
-git clone https://github.com/openai/gym.git /opt/openai-gym
+# # Install OpenAI in /opt (which is meant for OS unbundled packages)
+# git clone https://github.com/openai/gym.git /opt/openai-gym
 
-# Install dependencies
-cd /opt/openai-gym
-pip3 install -e '.[box2d]'
+# # Install dependencies
+# cd /opt/openai-gym
+# pip3 install -e '.[box2d]'
